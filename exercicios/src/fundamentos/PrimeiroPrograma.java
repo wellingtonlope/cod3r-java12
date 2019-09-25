0439cfad3f43384759a9ca1109eee13cb394fe64
Wellington Lopes <wellingtonlopes@tce.rr.leg.br>
2019-09-25 11:07:41 -0400

    comentarios

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,28 @@
 package fundamentos;
 
+/**
+ * Essa classe representa... JAVADOC
+ *
+ * @author Wellington Lopes <@email.com>
+ * @see
+ * @since 1.0
+ */
 public class PrimeiroPrograma {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
+        // Uma sentenças de código termina com ;
+        // Mais um comentário
+        // Fim
         System.out.println("Primeiro programa parte #01!");
+
+        /*
+            Linha 1
+            Linha 2
+         */
         System.out.println("Primeiro programa parte #02!");
+        System.out.println("Fim"); // Arqui também funciona!
     }
 }
