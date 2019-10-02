007eed021f0282f838c2d412095650e385eb9409
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-10-02 18:17:23 -0400

    desafio metodo obterDataFormatada

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -4,4 +4,11 @@ public class Data {
     Integer dia;
     Integer mes;
     Integer ano;
+
+    String obterDataFormatada() {
+        String diaS = dia < 10 ? '0' + dia.toString() : dia.toString();
+        String mesS = mes < 10 ? '0' + mes.toString() : mes.toString();
+        String anoS = ano.toString();
+        return String.format("%s/%s/%s", diaS, mesS, anoS);
+    }
 }
--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -12,7 +12,7 @@ public class DataTeste {
         d2.mes = 10;
         d2.ano = 2019;
 
-        System.out.printf("%d/%d/%d\n", d1.dia, d1.mes, d1.ano);
-        System.out.printf("%d/%d/%d\n", d2.dia, d2.mes, d2.ano);
+        System.out.println(d1.obterDataFormatada());
+        System.out.println(d2.obterDataFormatada());
     }
 }
