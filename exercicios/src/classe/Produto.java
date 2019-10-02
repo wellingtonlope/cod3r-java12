7bdb24cb82b43b4c5919ad1b5628217a706fd4e7
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-10-02 18:12:17 -0400

    medo precoComDesconto

--- exercicios/src/classe/Produto.java
+++ exercicios/src/classe/Produto.java
@@ -4,4 +4,12 @@ public class Produto {
     String nome;
     Double preco;
     Double desconto;
+
+    Double precoComDesconto() {
+        return preco * (1 - desconto);
+    }
+
+    Double precoComDesconto(double descontoDoGerente) {
+        return preco * (1 - desconto + descontoDoGerente);
+    }
 }
