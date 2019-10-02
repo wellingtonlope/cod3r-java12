c9a16b464c60e086ea8d28d18162e2aca7e0cefd
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-10-02 18:47:59 -0400

    desafio construtor

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -5,6 +5,18 @@ public class Data {
     Integer mes;
     Integer ano;
 
+    Data() {
+        dia = 1;
+        mes = 1;
+        ano = 1970;
+    }
+
+    Data(int diaInicial, int mesInicial, int anoInicial) {
+        dia = diaInicial;
+        mes = mesInicial;
+        ano = anoInicial;
+    }
+
     String obterDataFormatada() {
         String diaS = dia < 10 ? '0' + dia.toString() : dia.toString();
         String mesS = mes < 10 ? '0' + mes.toString() : mes.toString();
--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -3,14 +3,9 @@ package classe;
 public class DataTeste {
     public static void main(String[] args) {
         Data d1 = new Data();
-        d1.dia = 1;
-        d1.mes = 4;
-        d1.ano = 1997;
+        d1.ano = 2021;
 
-        var d2 = new Data();
-        d2.dia = 2;
-        d2.mes = 10;
-        d2.ano = 2019;
+        var d2 = new Data(1, 4, 1997);
 
         System.out.println(d1.obterDataFormatada());
         d2.imprimirDataFormatada();
