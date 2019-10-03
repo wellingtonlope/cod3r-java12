12660cdfea4bad9b10198e44f1344d10b47384a5
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-10-02 20:52:39 -0400

    this e this()

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -6,15 +6,13 @@ public class Data {
     Integer ano;
 
     Data() {
-        dia = 1;
-        mes = 1;
-        ano = 1970;
+        this(1,1,1970);
     }
 
-    Data(int diaInicial, int mesInicial, int anoInicial) {
-        dia = diaInicial;
-        mes = mesInicial;
-        ano = anoInicial;
+    Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
     }
 
     String obterDataFormatada() {
