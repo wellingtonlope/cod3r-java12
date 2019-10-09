cfb6fd7c67f2a528cc3de3875067da68f633aed8
Wellington Lopes <wellingtonlopes@tce.rr.leg.br>
2019-10-09 11:16:37 -0400

    Reduce 2

--- exercicios/src/streams/Aluno.java
+++ exercicios/src/streams/Aluno.java
@@ -3,9 +3,15 @@ package streams;
 public class Aluno {
     final String nome;
     final double nota;
+    final boolean bomComportamento;
 
     public Aluno(String nome, double nota) {
+        this(nome, nota, true);
+    }
+
+    public Aluno(String nome, double nota, boolean bomComportamento) {
         this.nome = nome;
         this.nota = nota;
+        this.bomComportamento = bomComportamento;
     }
 }
